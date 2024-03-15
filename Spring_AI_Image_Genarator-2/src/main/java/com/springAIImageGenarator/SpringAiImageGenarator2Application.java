bdbb334a7d648c0c51e3edaf8feac88a91230ad9
Jeevith <102320065+Jeevith-U@users.noreply.github.com>
2024-03-15 19:45:55 +0100

    Add files via upload

---
+++ Spring_AI_Image_Genarator-2/src/main/java/com/springAIImageGenarator/SpringAiImageGenarator2Application.java
@@ -0,0 +1,13 @@
+package com.springAIImageGenarator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiImageGenarator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiImageGenarator2Application.class, args);
+	}
+
+}
---
+++ Spring_AI_Image_Genarator-2/src/test/java/com/springAIImageGenarator/SpringAiImageGenarator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAIImageGenarator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiImageGenarator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
